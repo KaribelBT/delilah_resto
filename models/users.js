88ee701cb39318cab5b1da4e2f9eb4ba6c6bfc5f
KaribelBT <karibel.barco@gmail.com>
2020-05-28 12:40:17 -0300

    fix create user

--- models/users.js
+++ models/users.js
@@ -1,12 +1,34 @@
 class Users {
     create(sql, username, fullname, email, phone, address, password) {
         let resp = sql.query(
-            `INSERT INTO users (username, fullname, email, phone, address, password, admin, enable)
-            VALUES  ("${username}", "${fullname}", "${email}", ${phone}, "${address}", "${password}", false, true)
-            `, {
+            `INSERT INTO users (username, fullname, email, phone, address, password, admin, enable) 
+             VALUES (:username, :fullname, :email, :phone, :address, :password, :admin, :enable)`,
+            { replacements: {
+                username,
+                fullname,
+                email,
+                phone,
+                address,
+                password,
+                admin: false,
+                enable: true
+                }
+            })    
+        return resp
+    }
+
+    userExist(req, res, next) {
+        const { fullname, username, email } = req.body
+        let exist = sequilize.query(
+            `SELECT fullname, email FROM users WHERE fullname = ${fullname} AND email = ${username} AND email = ${email}`, {
+            type: sql.QueryTypes.SELECT,
             raw: true
         })
-        return resp
+        if (exist.length > 0) {
+            return res
+                .status(409)
+                .json({ error: `Ya existe el usuario ${fullname} asociado a este nombre ${username} y email ${email}` });
+        } else { next() };
     }
     list(sql) {
         let resp = sql.query(
@@ -16,7 +38,7 @@ class Users {
         })
         return resp
     }
-    get(sql,id) {
+    get(sql, id) {
         let resp = sql.query(
             `SELECT * FROM users WHERE id = ${id}`, {
             type: sql.QueryTypes.SELECT,
